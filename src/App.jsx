96796ec070f79d0c2816e2d087d652534ed6e04e
farismaulanaa12 <farismaulanaalbai@gmail.com>
2024-02-08 19:09:48 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavigationBar } from './components/navbar'
+
+export default function App() {
+  return (
+    <>
+      <NavigationBar/>
+      <div className='' style={{backgroundColor: '#d9d9d9'}}>
+        <section>
+          
+        </section>
+      </div>
+    </>
+  )
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const NavigationBar = () => {
+  return (
+    <>
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark text-center">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">History</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Type</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Habitat</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Gallery</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    </>
+  )
+}
